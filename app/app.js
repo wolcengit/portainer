a2b10bdc8851a7c4e1d73aeafd0c9b35e04cdfe7
xAt0mZ <baron_l@epitech.eu>
2019-06-27 00:54:38 +0200

    feat(registry): retag with progression + rewrite manifest REST service to jquery

--- app/app.js
+++ app/app.js
@@ -38,7 +38,7 @@ function ($rootScope, $state, LocalStorage, Authentication, authManager, StateMa
 
   $(document).ajaxSend(function (event, jqXhr, jqOpts) {
     const type = jqOpts.type === 'POST' || jqOpts.type === 'PUT' || jqOpts.type === 'PATCH';
-    const hasNoContentType = jqOpts.contentType !== 'application/json' && !jqOpts.headers['Content-Type'];
+    const hasNoContentType = jqOpts.contentType !== 'application/json' && jqOpts.headers && !jqOpts.headers['Content-Type'];
     if (type && hasNoContentType) {
       jqXhr.setRequestHeader('Content-Type', 'application/json');
     }
